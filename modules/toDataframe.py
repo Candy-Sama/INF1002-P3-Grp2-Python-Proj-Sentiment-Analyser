834c1eae41d1a9b8b6a0a849f719d7029366b9d9
Hxruya <87225287+Hxruya@users.noreply.github.com>
2025-09-27 01:07:02 +0800

    moved excel file into data

--- modules/toDataframe.py
+++ modules/toDataframe.py
@@ -1,15 +1,16 @@
 import os
 import pandas as pd
-import reviewScorer
+from reviewMethods import sentence_score_calculator
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(BASE_DIR, "..", "steam_reviews_315210.xlsx")
+file_path = os.path.join(BASE_DIR, "..", "data", "steam_reviews_315210.xlsx")
 
 df = pd.read_excel(file_path)
 
 review_column = df['review_text'].head(10)
 
 for review in review_column:
-    scores = reviewScorer.sentence_score_calculator(review)
+    scores = sentence_score_calculator(review)
     for i in range(len(scores)):
         output = scores[i]
+        print(output)
