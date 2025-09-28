d58559a5e99c24be271b3bb9c1e66f9317ad4701
Muhammad Mustaqim Bin Aziz <muspartan117@gmail.com>
2025-09-28 20:22:01 +0800

    Update data_to_frontend.py

--- modules/data_to_frontend.py
+++ modules/data_to_frontend.py
@@ -20,10 +20,10 @@ def get_reviews(file_path):
     outputList = []
     for eachReview in range(len(df['review_text'].head(10))):
         review_text = df['review_text'].iloc[eachReview]
-        review_text = re.sub(r'\n', ' ', review_text)  # Replace newlines with spaces
-        review_text = re.sub(r'_x000D_', ' ', review_text)  # Replace '_x000D_' with spaces
+        review_text = re.sub(r'\n', '', review_text)  # Replace newlines
+        review_text = re.sub(r'_x000D_', '', review_text)  # Replace '_x000D_'
         review_text = re.sub(r'\s+', ' ', review_text).strip()  # Replace multiple spaces with a single space
-        
+
         outputList.append({
             "review_id": df['review_id'].iloc[eachReview],
             "review_text": review_text
